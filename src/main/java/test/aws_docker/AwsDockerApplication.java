fc2da8885b0a23332127e474bad36c415a0acfbc
jjun522 <tjwowns522@>
2024-12-28 00:05:51 +0900

    프로젝트 세팅

---
+++ src/main/java/test/aws_docker/AwsDockerApplication.java
@@ -0,0 +1,13 @@
+package test.aws_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/aws_docker/AwsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.aws_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
